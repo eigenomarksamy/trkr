8d5b9fd8f86c1738f751af70231c6b94cc09220b
eigenomarksamy <omarkhaledsamy20@gmail.com>
2025-03-11 08:52:26 +0100

    add auto run script for windows

---
+++ run_script.bat
@@ -0,0 +1,18 @@
+@echo off
+REM Navigate to the script directory (if the batch script is run from another location)
+cd /d "%~dp0"
+
+REM Activate the virtual environment
+call .venv\Scripts\activate.bat
+
+REM Upgrade pip
+python -m pip install --upgrade pip
+
+REM Install required dependencies
+python -m pip install -r requirements.txt
+
+REM Run the Python script
+python run.py
+
+REM (Optional) Deactivate the virtual environment
+deactivate
